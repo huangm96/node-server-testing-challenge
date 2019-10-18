ca97981bbf9a0faa9406078bef3e34440a33ab16
Min-Huang <minh.huang96@gmail.com>
2019-10-17 18:58:06 -0700

    added test for insert()

--- users/usersModel.spec.js
+++ users/usersModel.spec.js
@@ -3,6 +3,33 @@ const db = require('../data/dbConfig.js')
 
 describe('users model', () => {
     it('should set testing environment', () => {
-        expect(true).toBeTruthy();
+       expect(process.env.DB_ENV).toBe("testing");
+    })
+})
+
+describe('insert()', () => {
+
+    beforeEach(async () => {
+        await db('users').truncate();
+})
+
+    it('should add user to database', async () => {
+        const records = await db('users');
+        expect(records).toHaveLength(0);
+
+        await usersModel.insert({ name: "Min" });
+        const usersRecord = await db('users');
+        expect(usersRecord).toHaveLength(1);
+    })
+})
+
+describe("getAll()", () => {
+    beforeEach(async () => {
+        await db('users').truncate();
+    })
+
+    it('should return users from database', async() => {
+        const usersNum = await db('users');
+        expect(usersNum).toHaveLength(0);
     })
 })
